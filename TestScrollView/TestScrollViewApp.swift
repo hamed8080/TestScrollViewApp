fe21c9737e06fbcbf400c320fd1146fd8beac9b8
Hamed Hosseini <hamed8080@gmail.com>
2023-07-05 14:54:22 +0330

    Initial Commit

---
+++ TestScrollView/TestScrollViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestScrollViewApp.swift
+//  TestScrollView
+//
+//  Created by hamed on 7/5/23.
+//
+
+import SwiftUI
+
+@main
+struct TestScrollViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
